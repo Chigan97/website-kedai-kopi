f0d165b169d88f911da58836a791fba4007681aa
chigan97 <alifichiganjati@gmail.com>
2023-05-14 15:59:34 +0700

    first commit

---
+++ Assets/Js/script.js
@@ -0,0 +1,8 @@
+// Toggle Class Active
+
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humburger di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active")
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# website-kedai-kopi
+# website-kedai-kopi
